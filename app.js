7872890e933fb6c49e97ffd7dad77edf21b525eb
Justin Ewalt <justinewalt@gmail.com>
2017-04-20 20:30:14 -0600

    doing the more things

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ console.log("We are on a new branch");
 
 var firstName = "Justin";
 var lastName = "Ewalt";
+var age = 28;
 
 console.log("Full Name: " + firstName + " " + lastName)
+
+if ( age > 19) {
+    console.log("Not a teen");
+} else {
+    console.log('little kid, homie.');
+}
